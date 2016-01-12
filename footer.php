98382e06e8cfe7c0ee80fe1058637608dc1e4302
mmonpresent <mon.khunmojung@gmail.com>
2016-01-12 10:39:32 +0700

    mon001

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php ?>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="js/ie10-viewport-bug-workaround.js"></script>
+<script src='js/table-fixed-header.js'></script>
+<script src='js/navbar-submenu.js'></script>
---
+++ header.php
@@ -0,0 +1,12 @@
+<?php ?>
+<!-- Bootstrap -->
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+ <link href="css/signin.css" rel="stylesheet">
+ <link href="css/custom.css" rel="stylesheet">
+ <script src="js/ie-emulation-modes-warning.js"></script>
+  
+ <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+ <!--[if lt IE 9]>
+   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+ <![endif]--> 
